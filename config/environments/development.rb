9ca3dd24b0bc8e557ca08c9cff3d3849b9cddffb
Daniel <dannyblaze469@gmail.com>
2021-11-11 22:17:23 -0500

    adding mail credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -87,4 +87,14 @@ Rails.application.configure do
   :authentication => 'plain',
   :enable_starttls_auto => true
   }
+
+#   config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = {
+#   address:              'smtp.gmail.com',
+#   port:                 587,
+#   domain:               'example.com',
+#   user_name:            '<username>',
+#   password:             '<password>',
+#   authentication:       'plain',
+#   enable_starttls_auto: true }
 end
